73691bb03bb4cb11537b05b2fd9b5588cd2578dc
deeplow <francisco@freedom.press>
2025-09-03 13:44:31 +0100

    Close xterm in precache_installation.pm

--- tests/securedrop/precache_installation.pm
+++ tests/securedrop/precache_installation.pm
@@ -42,6 +42,9 @@ sub run {
     assert_script_run('qvm-template install fedora-42-xfce', timeout => 1500);
     assert_script_run('qvm-template install debian-12-minimal', timeout => 1500);
     assert_script_run('qubes-vm-update --force-update --show-output', timeout => 3600);
+
+    send_key('alt-f4');  # close xterm
+
 }
 
 sub post_fail_hook {
